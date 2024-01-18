c4ba65d0f63f329add5c251907421bda9cd98667
Joseph Myers <josephlmyers@gmail.com>
2024-01-18 16:51:12 -0600

    fish and alacritty in a stable place

---
+++ fish/conf.d/omf.fish
@@ -0,0 +1,8 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH '/home/jm/.local/share/fish/omf'
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG '/home/jm/.config/fish/omf'
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,28 @@
+set -U EDITOR nvim
+set -xg ARCHFLAGS "-arch x86_64"
+
+source ~/.config/.bash_aliases
+[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
+# my favorite 'woopsie' slash undo or 'I meant to do' app of ALL time.
+# Theydies Laydies Gaydies and Gentlemanz, I present to you,
+# thefuck https://archlinux.org/packages/extra/any/thefuck/
+# TODO Make this work with fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+starship init fish | source
+
+# bun
+set -xg BUN_INSTALL "$HOME/.bun"
+set -xg PATH "$BUN_INSTALL/bin:$PATH"
+
+
+
+# TODO Where are my gem paths?
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+set -xg PATH "$PATH:$HOME/.rvm/bin"
+
+# Prompt Improvement Initiative
+
