99bb06c746ccb30270653fb6770aa41948fb2695
Joseph Myers <josephlmyers@gmail.com>
2024-01-18 18:19:03 -0600

    fish and rvm and jekyll now lubs me

--- fish/config.fish
+++ fish/config.fish
@@ -16,13 +16,16 @@ starship init fish | source
 
 # bun
 set -xg BUN_INSTALL "$HOME/.bun"
-set -xg PATH "$BUN_INSTALL/bin:$PATH"
-
-
-
-# TODO Where are my gem paths?
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-set -xg PATH "$PATH:$HOME/.rvm/bin"
+set -xg PATH "$PATH:$BUN_INSTALL/bin"
+# rvm
+set -xg RVM_PATH "$HOME/.rvm/bin"
+set -xg PATH "$PATH:$RVM_PATH"
+rvm default
+# ruby
+set -xg RUBY_PATH "$HOME/.rvm/rubies/ruby-3.2.2/lib/ruby/gems/3.2.0/plugins/"
+set -xg PATH "$PATH:$RUBY_PATH"
+# gems
+set -xg GEM_PATH "$HOME/.rvm/gems/ruby-3.2.2@global/"
+set -xg PATH "$PATH$GEM_PATH"
 
 # Prompt Improvement Initiative
-
