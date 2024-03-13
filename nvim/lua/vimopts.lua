4c4b9bb7150a8872094c5369a8ac3f2934a0206d
Joseph Myers <josephlmyers@gmail.com>
2024-03-13 16:44:53 -0500

    uproarious day trying to get tmux-resurrect to work.  Abandoned.

--- nvim/lua/vimopts.lua
+++ nvim/lua/vimopts.lua
@@ -4,8 +4,8 @@
 -- See `:help mapleader`
 -- Set all global options before loading any plugins
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -22,15 +22,19 @@ vim.opt.relativenumber = true
 -- Enable termguicolors for lualine
 vim.opt.termguicolors = true
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Don't show the mode, since it's already in status line
 vim.opt.showmode = false
 
+-- Turn on cursor rulers
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -43,7 +47,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -57,10 +61,10 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '󰌒 ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "󰌒 ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.opt.cursorline = true
@@ -73,11 +77,11 @@ vim.opt.hlsearch = true
 -- abstract away vim.opt code into submodule
 
 -- Set custom options for folding
-vim.opt.foldmethod = 'indent'
+vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 99
 
 --Custom autocommands
 --Unclear if this is working, I've yet to see undercurl
 --Undercurl
-vim.cmd [[let &t_Cs = "\e[4:3m"]]
-vim.cmd [[let &t_Ce = "\e[4:0m"]]
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
