72cd974cf8f857e390d3a7817445e61d5fcbb705
Joseph Myers <josephlmyers@gmail.com>
2024-03-14 11:47:04 -0500

    icon picker implemented and starship config tweak

--- nvim/lua/custom/plugins/icon_picker.lua
+++ nvim/lua/custom/plugins/icon_picker.lua
@@ -3,9 +3,41 @@ return {
 	config = function()
 		require("icon-picker").setup({ disable_legacy_commands = true })
 
-		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<Leader>ii", "<cmd>IconPickerNormal<cr>", opts)
-		vim.keymap.set("n", "<Leader>iy", "<cmd>IconPickerYank<cr>", opts)
-		vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
+		vim.keymap.set(
+			"n",
+			"<Leader>ia",
+			"<cmd>IconPickerNormal<cr>",
+			{ noremap = true, silent = true, desc = "Pick [A]ll" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>ie",
+			"<cmd>IconPickerNormal nerd_font_v3 emoji<cr>",
+			{ noremap = true, silent = true, desc = "Pick NerdFonts & [E]moji" }
+		)
+		vim.keymap.set(
+			"n",
+			"<Leader>il",
+			"<cmd>IconPickerNormal alt_font symbols<cr>",
+			{ noremap = true, silent = true, desc = "Pick A[l]t Fonts & Symbols" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>in",
+			"<cmd>IconPickerNormal nerd_font_v3<cr>",
+			{ noremap = true, silent = true, desc = "Pick [N]erdFonts" }
+		)
+		vim.keymap.set(
+			"n",
+			"<Leader>iy",
+			"<cmd>IconPickerYank<cr>",
+			{ noremap = true, silent = true, desc = "[Y]ank Icon into Register" }
+		)
+		vim.keymap.set(
+			"i",
+			"<C-i>",
+			"<cmd>IconPickerInsert<cr>",
+			{ noremap = true, silent = true, desc = "[I]nsert Icon" }
+		)
 	end,
 }
