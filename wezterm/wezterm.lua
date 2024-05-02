c71d94f64af8af68eb2e455100959b1f4703b61c
Joseph Myers <josephlmyers@gmail.com>
2024-05-02 01:50:52 -0500

    more wezterm tweaks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,8 +10,24 @@ config.color_scheme = "Catppuccin Macchiato (Gogh)"
 config.font = wezterm.font("0xProto Nerd Font Mono")
 config.font_size = 16
 
+config.adjust_window_size_when_changing_font_size = false
+
+config.window_padding = {
+	left = "1cell",
+	right = "0.5cell",
+	top = "0.5cell",
+	bottom = 0,
+}
+
+config.visual_bell = {
+	fade_in_duration_ms = 75,
+	fade_out_duration_ms = 75,
+	target = "CursorColor",
+}
+
 config.enable_tab_bar = false
 
 config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
 
 return config
