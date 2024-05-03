7704df18d3cb5174e1952245a26228ec456f8310
Joseph Myers <josephlmyers@gmail.com>
2024-05-02 20:28:33 -0500

    more wezterm tweaks

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,7 +7,10 @@ local config = wezterm.config_builder()
 config.keys = {}
 
 config.color_scheme = "Catppuccin Macchiato (Gogh)"
-config.font = wezterm.font("0xProto Nerd Font Mono")
+config.font = wezterm.font_with_fallback({
+	"Liberation Mono",
+	{ family = "Symbols Nerd Font Mono", scale = 0.8 },
+})
 config.font_size = 16
 
 config.adjust_window_size_when_changing_font_size = false
@@ -16,7 +19,7 @@ config.window_padding = {
 	left = "1cell",
 	right = "0.5cell",
 	top = "0.5cell",
-	bottom = 0,
+	bottom = "0cell",
 }
 
 config.visual_bell = {
