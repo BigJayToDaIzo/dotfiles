f4c21b04dc7a7ed03973f09864efafe9b0102fa2
Joseph Myers <josephlmyers@gmail.com>
2024-08-04 19:46:29 -0500

    yazi colors seem busted again

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,30 +1,33 @@
 return {
-  -- NOTE: Welcome to my FAVORITE (and only) yazi-nvim plugin
-  -- I invite you to join me using yazi as a file tree.  It's like magic 
-  -- and feels so vimly natural and buttery.  But as always.  Feel FREE
-  -- to use Neo-tree or Netrw or whatever your heart desires here.
-  -- I found it a soft place to land as a new learner.
-	"mikavilpas/yazi.nvim",
-  event = "VeryLazy",
-	keys = {
-		{
-			"<leader>e",
-			function()
-				require("yazi").yazi(nil, vim.fn.getcwd())
-			end,
-			desc = "[E]xecute Yazi File Tree 󰙅 ",
+	{
+		-- NOTE: Welcome to my FAVORITE (and only) yazi-nvim plugin
+		-- I invite you to join me using yazi as a file tree.  It's like magic
+		-- and feels so vimly natural and buttery.  But as always.  Feel FREE
+		-- to use Neo-tree or Netrw or whatever your heart desires here.
+		-- I found it a soft place to land as a new learner.
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>e",
+				function()
+					require("yazi").yazi(nil, vim.fn.getcwd())
+				end,
+				desc = "[E]xecute Yazi File Tree 󰙅 ",
+			},
+		},
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = true,
+			floating_window_scaling_factor = 0.85,
+			-- bugged for me, does weird things with icons
+			-- This seems to be deeply nested in a popular tui library
+			-- outside of the control of the plugin author, neovim
+			-- or any muxer you may be using (tmux, zellij, etc)
+			yazi_floating_window_winblend = 15,
+			use_ya_for_events_reading = true,
+			use_yazi_client_id_flag = true,
 		},
 	},
-	opts = {
-		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = true,
-		floating_window_scaling_factor = 0.85,
-    -- bugged for me, does weird things with icons
-    -- This seems to be deeply nested in a popular tui library
-    -- outside of the control of the plugin author, neovim
-    -- or any muxer you may be using (tmux, zellij, etc)
-    -- yazi_floating_window_winblend = 15,
-    use_ya_for_events_reading = true,
-    use_yazi_client_id_flag = true,
-	},
+	-- Other yazi plugins?  The ecosystem is rich!
 }
