34eaf89221ef7ac9858dcb0bd9c00de2290762aa
Joseph Myers <josephlmyers@gmail.com>
2024-08-06 00:11:02 -0500

    VimBeGood easter egg

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -1,5 +1,8 @@
 return {
 	-- NOTE: As always, use what you want.  But LazyGit is worth a try!
+	-- LOADS of configuration options.  I like vanilla
+	-- :h lazygit<C-d>
+	-- :LazyG<C-d>
 	"kdheepak/lazygit.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
---
+++ nvim/lua/plugins/vimbegood.lua
@@ -0,0 +1,4 @@
+return {
+	-- Lil game to help u git good at vim motions!
+	"ThePrimeagen/vim-be-good",
+}
