d2b57c84ec855bec307952f0b56fc8dc1cc01af8
Joseph Myers <josephlmyers@gmail.com>
2024-11-15 22:50:12 -0600

    update oil to use focus commands to split and removed tabline from kit

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -6,8 +6,14 @@ return {
 	vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { desc = "Open Oil" }),
 	config = function()
 		require("oil").setup({
+			keymaps = {
+				-- override defaults to use focus plugin commands
+				vim.keymap.set("n", "<C-s>", "<CMD>FocusSplitRight<CR>", { desc = "Focus Split Right" }),
+				vim.keymap.set("n", "<C-v>", "<CMD>FocusSplitDown<CR>", { desc = "FloatSplitDown" }),
+			},
 			float = {
 				padding = 10,
+				preview_split = "below",
 			},
 		})
 	end,
