cbe5b214da48f7ea01eb149e066e0beedb96bb65
Joseph Myers <josephlmyers@gmail.com>
2024-11-21 14:12:46 -0600

    added xray and tweak copilot

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -84,7 +84,9 @@ return {
 		-- -settings (table): Override the default settings passed when initializing the server.
 		-- For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			clangd = {},
+			clangd = {
+				filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "hpp" },
+			},
 			gopls = { "ast-grep", "golangci-lint-langserver", "gopls", "templ" },
 			-- pyright = {},
 			rust_analyzer = {},
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,7 +5,59 @@ return {
 	},
 	config = function()
 		require("lualine").setup({
-			theme = "everforest",
+			options = {
+				theme = "everforest",
+			},
+			sections = {
+				lualine_a = {
+					"mode",
+					{
+						icons_enabled = true,
+					},
+				},
+				lualine_b = {
+					"branch",
+					"diff",
+					{
+						"diagnostics",
+						sources = { "nvim_diagnostic" },
+						symbols = { error = " ", warn = " ", info = " ", hint = " " },
+					},
+				},
+				lualine_c = { "filename" },
+				lualine_x = {
+					{
+						"copilot",
+						symbols = {
+							status = {
+								icons = {
+									enabled = " ",
+									sleep = "󰒲 ", -- auto-trigger disabled
+									disabled = " ",
+									warning = " ",
+									unknown = " ",
+								},
+								hl = {
+									enabled = "#50FA7B",
+									sleep = "#AEB7D0",
+									disabled = "#6272A4",
+									warning = "#FFB86C",
+									unknown = "#FF5555",
+								},
+							},
+							spinners = require("copilot-lualine.spinners").dots,
+							spinner_color = "#6272A4",
+						},
+						show_colors = true,
+						show_loading = true,
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
 		})
 	end,
 }
