f87f4c4a5bd9c1ab2362ef222623252ce0051724
Joseph Myers <josephlmyers@gmail.com>
2024-12-14 11:34:26 -0600

    status line on and configs stable

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,14 +1,27 @@
 return {
-	"stevearc/oil.nvim",
-	dependencies = {
-		{ "echasnovski/mini.icons", opts = {} },
-	},
-	vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { desc = "Open Oil" }),
-	config = function()
-		require("oil").setup({
-			float = {
-				padding = 10,
-			},
-		})
-	end,
+  {
+    "stevearc/oil.nvim",
+    dependencies = {
+      { "echasnovski/mini.icons", opts = {} },
+    },
+    vim.keymap.set("n", "<leader>e", "<CMD>Oil --float<CR>", { desc = "Open Oil" }),
+    config = function()
+      require("oil").setup({
+        win_options = {
+          signcolumn= "yes:2",
+        },
+        float = {
+          padding = 5,
+        },
+      })
+    end,
+  },
+  {
+    "refractalize/oil-git-status.nvim",
+    dependencies = {
+      "stevearc/oil.nvim",
+    },
+    opts = {},
+
+  },
 }
---
+++ nvim/plugintodo.md
@@ -0,0 +1,31 @@
+Mason
+    Neodev.nvim nvim-cmp section post nvim-cmp install 
+LSP:lspconfig & mason-lspconfig.nvim 
+FORMAT: conform & mason-conform 
+LINT:nvim-lint & mason-nvim-lint 
+DAP:nvim-dap & nvim-dap-ui & nvim-neotest/nvim-nio & nvim-dap-virtual-text & mason-nvim-dap.nvim 
+NEOTEST 
+Trouble 
+NeoCodium?? (Bench Copilot) 
+GoQoL 
+Notifications 
+StatusLine 
+Flash 
+Treesitter 
+    Treesitter related modules like: Highlight, Incremental selection, Indentation, Folding 
+    Treesitter related plugins like: nvim-treesitter-context nvim-treesitter-textobjects 
+Telescope 
+    Telescope Plugins? 
+Copilot 
+Completion: Snippets implement 
+lazydev 
+IndentBlankline 
+Git: 
+LazyGit 
+Gitsigns 
+Fidget 
+Precognition 
+Hardtime
+Windline
+~~Oil File-Tree~~
+~~Oil-Git-Status~~
